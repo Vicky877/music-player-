761e809903c4e0af58a5d2f7fdc5ceb743e82aff
Vignesh M <65291705+Vicky877@users.noreply.github.com>
2021-08-09 10:26:59 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,184 @@
+document.body.addEventListener('keydown', function(e) {
+    var keycode = e.keyCode;
+    if (keycode === 65) {
+        var btn = document.getElementById('a');
+        btn.classList.add('button1');
+        var audio = document.getElementById('my');
+        audio.play();
+
+    }
+
+
+}, );
+document.body.addEventListener('keyup', function(e) {
+    var keycode = e.keyCode;
+    if (keycode === 65) {
+        var btn = document.getElementById('a');
+        btn.classList.remove('button1');
+        var audio = document.getElementById('my');
+        audio.pause();
+
+    }
+
+
+}, );
+document.body.addEventListener('keydown', function(s) {
+    var k = s.keyCode;
+    if (k === 83) {
+        // var sec = document.getElementById('sec');
+        var secbtn = document.getElementById('s');
+        secbtn.classList.add('button1');
+        var sec = document.getElementById('second');
+        sec.play();
+    }
+});
+document.body.addEventListener('keyup', function(s) {
+    var k = s.keyCode;
+    if (k === 83) {
+
+        var secbtn = document.getElementById('s');
+        secbtn.classList.remove('button1');
+        var sec = document.getElementById('second');
+        sec.pause();
+    }
+});
+document.body.addEventListener('keydown', function(d) {
+    var ky = d.keyCode;
+    if (ky === 68) {
+
+        var thbtn = document.getElementById('d');
+        thbtn.classList.add('button1');
+        var thrid = document.getElementById('third');
+        thrid.play();
+    }
+});
+document.body.addEventListener('keyup', function(d) {
+    var ky = d.keyCode;
+    if (ky === 68) {
+        // var sec = document.getElementById('sec');
+        var thbtn = document.getElementById('d');
+        thbtn.classList.remove('button1');
+        var third = document.getElementById('third');
+        third.pause();
+    }
+});
+document.body.addEventListener('keydown', function(d) {
+    var kyf = d.keyCode;
+    if (kyf === 70) {
+
+        var thbtn = document.getElementById('f');
+        thbtn.classList.add('button1');
+        var fouth = document.getElementById('fouth');
+        fouth.play();
+    }
+});
+document.body.addEventListener('keyup', function(d) {
+    var kyf = d.keyCode;
+    if (kyf === 70) {
+        // var sec = document.getElementById('sec');
+        var fobtn = document.getElementById('f');
+        fobtn.classList.remove('button1');
+        var fouth = document.getElementById('fouth');
+        third.pause();
+    }
+});
+document.body.addEventListener('keydown', function(d) {
+    var kyg = d.keyCode;
+    if (kyg === 71) {
+
+        var ftbtn = document.getElementById('g');
+        ftbtn.classList.add('button1');
+        var fiveth = document.getElementById('fiveth');
+        fiveth.play();
+    }
+});
+document.body.addEventListener('keyup', function(d) {
+    var kyg = d.keyCode;
+    if (kyg === 71) {
+        // var sec = document.getElementById('sec');
+        var ftbtn = document.getElementById('g');
+        ftbtn.classList.remove('button1');
+        var fiveth = document.getElementById('fiveth');
+        fiveth.pause();
+    }
+});
+document.body.addEventListener('keydown', function(d) {
+    var kyh = d.keyCode;
+    if (kyh === 72) {
+
+        var ftbtn = document.getElementById('h');
+        ftbtn.classList.add('button1');
+        var sixth = document.getElementById('sixth');
+        sixth.play();
+    }
+});
+document.body.addEventListener('keyup', function(d) {
+    var kyh = d.keyCode;
+    if (kyh === 72) {
+        // var sec = document.getElementById('sec');
+        var ftbtn = document.getElementById('h');
+        ftbtn.classList.remove('button1');
+        var sixth = document.getElementById('sixth');
+        sixth.pause();
+    }
+});
+document.body.addEventListener('keydown', function(d) {
+    var kyj = d.keyCode;
+    if (kyj === 74) {
+
+        var seth = document.getElementById('j');
+        seth.classList.add('button1');
+        var seveth = document.getElementById('seveth');
+        seveth.play();
+    }
+});
+document.body.addEventListener('keyup', function(d) {
+    var kyj = d.keyCode;
+    if (kyj === 74) {
+        // var sec = document.getElementById('sec');
+        var sebtn = document.getElementById('j');
+        sebtn.classList.remove('button1');
+        var seveth = document.getElementById('seveth');
+        seveth.pause();
+    }
+});
+document.body.addEventListener('keydown', function(d) {
+    var kyj = d.keyCode;
+    if (kyj === 75) {
+
+        var seth = document.getElementById('k');
+        seth.classList.add('button1');
+        var seveth = document.getElementById('egith');
+        seveth.play();
+    }
+});
+document.body.addEventListener('keyup', function(d) {
+    var kyj = d.keyCode;
+    if (kyj === 75) {
+        // var sec = document.getElementById('sec');
+        var sebtn = document.getElementById('k');
+        sebtn.classList.remove('button1');
+        var seveth = document.getElementById('egith');
+        seveth.pause();
+    }
+});
+document.body.addEventListener('keydown', function(d) {
+    var kyj = d.keyCode;
+    if (kyj === 76) {
+
+        var seth = document.getElementById('l');
+        seth.classList.add('button1');
+        var seveth = document.getElementById('nine');
+        seveth.play();
+    }
+});
+document.body.addEventListener('keyup', function(d) {
+    var kyj = d.keyCode;
+    if (kyj === 76) {
+        // var sec = document.getElementById('sec');
+        var sebtn = document.getElementById('l');
+        sebtn.classList.remove('button1');
+        var seveth = document.getElementById('nine');
+        seveth.pause();
+    }
+});
